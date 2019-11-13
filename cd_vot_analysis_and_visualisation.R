0cb0e084a5a1e23ad442952c232651ffd6e38be2
George Moroz <agricolamz@gmail.com>
2019-11-13 18:39:57 +0300

    add new graphs

--- cd_vot_analysis_and_visualisation.R
+++ cd_vot_analysis_and_visualisation.R
@@ -1,6 +1,6 @@
-setwd("/home/agricolamz/work/articles/2019_Abaza_VOT_Mamonova")
+setwd("/home/agricolamz/work/articles/2019_Abaza_VOT_Mamonova/github_repo")
 library(tidyverse)
-df <- read_csv("result_all.csv")
+df <- read_csv("result_all_v2.csv")
 
 
 df %>% 
@@ -34,13 +34,14 @@ df %>%
                          cons == "кI" ~ "ejective", 
                          cons == "хъ" ~ "plain", 
                          cons == "къ" ~ "ejective"),
-         poa = factor(poa, c("labial", "dental", "palatal", "velar", "uvular"))) %>% 
+         poa = factor(poa, c("labial", "dental", "palatal", "velar", "uvular")),
+         phonation = factor(phonation, c("plain", "ejective", "voiced"))) %>% 
   select(-start_time, -end_time) %>% 
   group_by(speaker, utterance, word) %>% 
   mutate(sum = sum(duration)) %>% 
   filter(!is.na(cons)) %>% 
   ungroup() %>% 
-  slice(-c(3846, 3850, 3849, 3851, 3852, 3847)) %>% 
+  slice(-c(3180, 3181, 3183, 3184, 3185, 3186)) %>% 
   spread(annotation, duration, fill = NA) %>% 
   mutate(cd_n = cd/sum,
          vot_n = vot/sum) ->
@@ -59,3 +60,56 @@ data %>%
   facet_grid(poa~speaker, scales = "free")+
   stat_ellipse()+
   labs(title = "Values of CD and VOT normolized with adjacent vowels")
+
+
+data %>% 
+  filter(poa != "velar") %>% 
+  mutate(vot = ifelse(str_detect(cons, "[бдг]"), -vot, vot)) ->
+  data_for_glm
+
+data_for_glm %>% 
+  ggplot(aes(poa, vot, fill = phonation))+
+  geom_violin()+
+  facet_wrap(~speaker)
+library(lme4)
+library(lmerTest)
+fit <- lmer(vot~poa+phonation+(1|speaker)+(1|utterance)+(1|word), data = data_for_glm)
+summary(fit)
+
+library(tidyverse)
+as.matrix(cf) %>% 
+  as.data.frame() %>% 
+  mutate(names = rownames(.)) %>% 
+  slice(-c(1:3)) %>% 
+  mutate(fit = fit@beta) %>% 
+  select(names, fit, `2.5 %`, `97.5 %`)
+
+library(effects)
+summary(fit)
+plot(allEffects(fit))
+
+data %>% 
+  filter(poa != "velar") %>% 
+  mutate(cons = case_when(
+    cons == "б" ~ "b",
+    cons == "п" ~ "p",
+    cons == "пI" ~ "p'",
+    cons == "д" ~ "d",
+    cons == "т" ~ "t",
+    cons == "тI" ~ "t'",
+    cons == "гь" ~ "gʲ",
+    cons == "кь" ~ "kʲ",
+    cons == "кIь" ~ "k'ʲ",
+    cons == "хъ" ~ "q",
+    cons == "къ" ~ "q'")) %>% 
+  count(cons, utterance, speaker) %>% 
+  spread(utterance, n) %>% 
+  arrange(speaker) %>% 
+  select(-u4, -u5) %>% 
+  pivot_longer(names_to = "utterance", values_to = "number", cf:u3) %>% 
+  ggplot(aes(cons, number, label = number, fill = utterance))+
+  geom_col(position = "dodge")+
+  facet_wrap(~speaker)+
+  theme_bw()
+  
+  
